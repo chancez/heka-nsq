7224568740f6a664db724100858f3947bc4392ce
Chance Zibolski <chance.zibolski@gmail.com>
2014-08-22 13:06:30 -0700

    Handle errors when publishing. Quit after too many attempts

--- output.go
+++ output.go
@@ -16,33 +16,38 @@ const (
 )
 
 type NsqOutputConfig struct {
-	Addresses []string `toml:"addresses"`
-	Topic     string   `toml:"topic"`
-	Mode      string   `toml:"mode"`
+	Addresses      []string `toml:"addresses"`
+	Topic          string   `toml:"topic"`
+	Mode           string   `toml:"mode"`
+	RetryDelay     uint64   `toml:"retry_delay"`
+	RetryQueueSize uint64   `toml:"retry_queue_size"`
+	MaxReconnects  uint64   `toml:"max_reconnects"`
+	MaxMsgRetries  uint64   `toml:"max_msg_retries"`
 }
 
 type NsqOutput struct {
 	*NsqOutputConfig
-	Mode      int
-	runner    pipeline.OutputRunner
-	producers map[string]*nsq.Producer
-	config    *nsq.Config
-	respChan  chan *nsq.ProducerTransaction
-	counter   uint64
-	hostPool  hostpool.HostPool
-	retryChan chan RetryMsg
+	Mode       int
+	runner     pipeline.OutputRunner
+	producers  map[string]*nsq.Producer
+	config     *nsq.Config
+	respChan   chan *nsq.ProducerTransaction
+	counter    uint64
+	hostPool   hostpool.HostPool
+	retryChan  chan RetryMsg
+	retryCount uint64
 }
 
 type RetryMsg struct {
 	count     uint64
 	maxCount  uint64
-	body      []byte
+	Body      []byte
 	retryChan chan RetryMsg
 }
 
 func (m RetryMsg) Retry() error {
 	if m.count > m.maxCount {
-		return errors.New("Exceeded max retry attempts")
+		return errors.New("Exceeded max retry attempts sending message. No longer requeuing message.")
 	}
 	m.retryChan <- m
 	m.count++
@@ -50,14 +55,17 @@ func (m RetryMsg) Retry() error {
 }
 
 func (output *NsqOutput) ConfigStruct() interface{} {
-	return &NsqOutputConfig{}
+	return &NsqOutputConfig{
+		RetryDelay:     3,
+		RetryQueueSize: 50,
+		MaxReconnects:  3,
+	}
 }
 
 func (output *NsqOutput) Init(config interface{}) (err error) {
 	conf := config.(*NsqOutputConfig)
 	output.NsqOutputConfig = conf
 	output.config = nsq.NewConfig()
-	// output.config.BackoffMultiplier = time.Duration(time.Second * 6)
 
 	switch conf.Mode {
 	case "round-robin":
@@ -73,6 +81,7 @@ func (output *NsqOutput) Init(config interface{}) (err error) {
 		if err != nil {
 			break
 		}
+		producer.SetLogger(nil, nsq.LogLevelError)
 		output.producers[addr] = producer
 	}
 
@@ -88,7 +97,7 @@ func (output *NsqOutput) Init(config interface{}) (err error) {
 	}
 
 	output.respChan = make(chan *nsq.ProducerTransaction, len(output.Addresses))
-	output.retryChan = make(chan RetryMsg, 50)
+	output.retryChan = make(chan RetryMsg, output.RetryQueueSize)
 	output.hostPool = hostpool.New(output.Addresses)
 	for i := 0; i < len(output.Addresses); i++ {
 		go output.responder()
@@ -106,52 +115,73 @@ func (output *NsqOutput) Run(runner pipeline.OutputRunner,
 	var (
 		pack     *pipeline.PipelinePack
 		outgoing []byte
+		delay    <-chan time.Time
+		msg      RetryMsg
 	)
 
 	output.runner = runner
-	defer close(output.respChan)
-	defer close(output.retryChan)
 	inChan := runner.InChan()
 	ok := true
 
+	defer output.cleanup()
+
 	for ok {
+		if delay != nil {
+			<-delay
+			delay = nil
+		}
 		select {
 		case pack, ok = <-inChan:
 			if !ok {
-				return
+				return nil
 			}
 			outgoing, err = output.runner.Encode(pack)
 			if err != nil {
-				return
-			}
-
-			err = output.sendMessage(outgoing)
-			if err != nil {
 				runner.LogError(err)
-				msg := RetryMsg{body: outgoing, retryChan: output.retryChan, maxCount: 3}
-				err = msg.Retry()
+			} else {
+				err = output.sendMessage(outgoing)
 				if err != nil {
-					runner.LogError(fmt.Errorf("%s. No longer attempting to send message.", err.Error()))
+					delay, err = output.handleSendErr(err)
+					if err != nil {
+						return
+					}
+					// Create a retry msg, and requeue it
+					msg := RetryMsg{Body: outgoing, retryChan: output.retryChan, maxCount: output.MaxMsgRetries}
+					err = msg.Retry()
+					if err != nil {
+						output.runner.LogError(err)
+					}
 				}
 			}
 			pack.Recycle()
-		case msg := <-output.retryChan:
-			err = output.sendMessage(msg.body)
+		case msg, ok = <-output.retryChan:
+			if !ok {
+				return nil
+			}
+			err = output.sendMessage(msg.Body)
 			if err != nil {
-				runner.LogError(err)
+				delay, err = output.handleSendErr(err)
+				if err != nil {
+					return
+				}
+				// requeue the message
 				err = msg.Retry()
 				if err != nil {
-					runner.LogError(fmt.Errorf("%s. No longer attempting to send message.", err.Error()))
+					output.runner.LogError(err)
 				}
 			}
 		}
 	}
 
+	return nil
+}
+
+func (output *NsqOutput) cleanup() {
+	close(output.respChan)
+	close(output.retryChan)
 	for _, producer := range output.producers {
 		producer.Stop()
 	}
-
-	return nil
 }
 
 // sendMessage asyncronously publishes encoded pipeline packs to nsq. It will
@@ -171,7 +201,21 @@ func (output *NsqOutput) sendMessage(body []byte) (err error) {
 		p := output.producers[hostPoolResponse.Host()]
 		err = p.PublishAsync(output.Topic, body, output.respChan, body, hostPoolResponse)
 	}
+	if err != nil {
+		output.retryCount++
+	} else {
+		output.retryCount = 0
+	}
 	return
+}
+
+func (output *NsqOutput) handleSendErr(err error) (<-chan time.Time, error) {
+	output.runner.LogError(err)
+	if output.retryCount >= output.MaxReconnects {
+		return nil, errors.New("Exceeded max reconnect attempts")
+	}
+	// how long we wait before trying to publish again
+	return time.After(time.Second * time.Duration(output.RetryDelay)), nil
 }
 
 // responder handles the eventual response from the asyncronous publish. If
@@ -205,7 +249,7 @@ func (output *NsqOutput) responder() {
 
 		if !success {
 			output.runner.LogError(fmt.Errorf("Publishing message failed: %s", t.Error.Error()))
-			msg := RetryMsg{body: body, retryChan: output.retryChan, maxCount: 3}
+			msg := RetryMsg{Body: body, retryChan: output.retryChan, maxCount: output.MaxMsgRetries}
 			err := msg.Retry()
 			if err != nil {
 				output.runner.LogError(fmt.Errorf("%s. No longer attempting to send message.", err.Error()))
